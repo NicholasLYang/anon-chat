1e7956d5795345afc036468abc7c62bbd17ceccb
Nicholas Yang <ny585@nyu.edu>
2019-05-24 14:54:20 -0500

    rearranged the MessagesArea component to be a little easier to refactor

--- client/src/components/MessagesArea.tsx
+++ client/src/components/MessagesArea.tsx
@@ -1,33 +1,32 @@
 import React from "react";
-import {Conversation, Message} from "../types";
+import { Conversation, Message } from "../types";
 
 interface Props {
   conversation: Conversation | undefined;
 }
 
-const MessagesArea: React.FunctionComponent<Props> = ({
-  conversation
-}) => {
+const MessagesArea: React.FunctionComponent<Props> = ({ conversation }) => {
   if (!conversation) {
     return null;
   }
   const { title, messages } = conversation;
+  const sortedMessages = sortMessages(messages);
   return (
     <div className="messagesArea">
       <h2>{title}</h2>
-      <ul>{sortMessages(messages)}</ul>
+      <ul>
+        {sortedMessages.map(message => {
+          return <li key={message.id}>{message.text}</li>;
+        })}
+      </ul>
     </div>
   );
 };
 
 const sortMessages = (messages: Message[]) => {
-  const sortedMessages = messages.sort(
+  return messages.sort(
     (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
   );
-  return sortedMessages.map(message => {
-    return <li key={message.id}>{message.text}</li>;
-  });
 };
-
 
 export default MessagesArea;
