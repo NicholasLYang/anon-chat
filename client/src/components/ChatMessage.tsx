989d3e758da265320baa3cfb0c1186c41feb34aa
Anthony Diké <anthonydike@nyu.edu>
2019-06-16 13:53:56 -0400

    Added iMessage message styling

--- client/src/components/ChatMessage.tsx
+++ client/src/components/ChatMessage.tsx
@@ -2,21 +2,60 @@ import React from "react";
 
 interface Props {
   text: string;
+  userIndex: number;
 }
 
-const messageStyle = {
-  display: "flex",
-  flexDirection: "column" as "column",
-  width: "200px",
-  alignItems: "flex-end",
-  padding: "10px",
-  borderRadius: "10px",
-  backgroundColor: "#09f",
-  margin: "10px"
+const getMessageStyle = (userIndex: number) => {
+  if (userIndex === 0) {
+    return { backgroundColor: "#ededed", color: "black" };
+  }
+  if (userIndex === 1) {
+    return { backgroundColor: "#09f", color: "white" };
+  }
+  return { backgroundColor: "white", color: "black" };
 };
 
-const ChatMessage: React.FunctionComponent<Props> = ({ text }) => {
-  return <div style={messageStyle}>{text}</div>;
+// const messageStyle = {
+//   display: "flex",
+//   flexDirection: "column" as "column",
+//   width: "200px",
+//   alignItems: "flex-end",
+//   padding: "10px",
+//   borderRadius: "10px",
+//   backgroundColor: "#09f",
+//   margin: "10px"
+// };
+
+// style={{
+//         ...getMessageStyle(userIndex),
+//         display: "flex",
+//         flexDirection: "column" as "column",
+//         width: "200px",
+//         alignItems: "flex-end",
+//         padding: "10px",
+//         borderRadius: "10px",
+//         margin: "10px"
+//       }}
+
+const ChatMessage: React.FunctionComponent<Props> = ({ text, userIndex }) => {
+  return (
+    <div>
+      <div
+        style={{
+          ...getMessageStyle(userIndex),
+          display: "flex",
+          flexDirection: "column" as "column",
+          width: "200px",
+          alignItems: "flex-end",
+          padding: "10px",
+          borderRadius: "10px",
+          margin: "10px"
+        }}
+      >
+        {text}
+      </div>
+    </div>
+  );
 };
 
 export default ChatMessage;
--- client/src/components/ChatMessages.tsx
+++ client/src/components/ChatMessages.tsx
@@ -1,6 +1,6 @@
-import React, { CSSProperties } from 'react';
-import { Conversation, Message } from '../types';
-import ChatMessage from './ChatMessage';
+import React, { CSSProperties } from "react";
+import { Conversation, Message } from "../types";
+import ChatMessage from "./ChatMessage";
 
 interface Props {
   conversation: Conversation;
@@ -8,18 +8,18 @@ interface Props {
   uuid: string;
 }
 
-const getMessageStyle = (
-  message: Message,
-  userIndex: number
-): CSSProperties => {
-  if (message.userIndex === -1) {
-    return { backgroundColor: 'darkgray', color: 'white' };
-  }
-  if (userIndex === message.userIndex) {
-    return { backgroundColor: 'blue', color: 'white' };
-  }
-  return { backgroundColor: 'white', color: 'black' };
-};
+// const getMessageStyle = (
+//   message: Message,
+//   userIndex: number
+// ): CSSProperties => {
+//   if (message.userIndex === -1) {
+//     return { backgroundColor: "darkgray", color: "white" };
+//   }
+//   if (userIndex === message.userIndex) {
+//     return { backgroundColor: "blue", color: "white" };
+//   }
+//   return { backgroundColor: "white", color: "black" };
+// };
 
 const ChatMessages: React.FunctionComponent<Props> = ({
   conversation,
@@ -35,7 +35,13 @@ const ChatMessages: React.FunctionComponent<Props> = ({
       <h2>{title}</h2>
       <div>
         {sortedMessages.map(message => {
-          return <ChatMessage key={message.id} text={message.text} />;
+          return (
+            <ChatMessage
+              userIndex={userIndex}
+              key={message.id}
+              text={message.text}
+            />
+          );
         })}
       </div>
     </div>
